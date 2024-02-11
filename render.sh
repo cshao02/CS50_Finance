8f0dbd3a36be0e49db4014eb4e2d1f12357b001d
cshao <82189093+cshao02@users.noreply.github.com>
2024-02-12 01:38:07 +0800

    Update render.sh

--- render.sh
+++ render.sh
@@ -2,6 +2,7 @@
 # exit on error
 set -o errexit
 
+export API_KEY=pk_e46347831c69417cb9db6e6878707c5d
 export FLASK_APP=app.py
 pip install -r requirements.txt
-export API_KEY=pk_e46347831c69417cb9db6e6878707c5d
+
